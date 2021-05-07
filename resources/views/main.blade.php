7a080f42cc2c302ef8b4930ca33ee49e58c84310
Dmitry <DMon-git@yandex.ru>
2021-05-07 16:52:44 +0500

    Main page chnge width div.

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -3,8 +3,8 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
+        <div class="col-md-10">
+            <div class="card" id="div_posts">
                 Тут будут посты
             </div>
         </div>
