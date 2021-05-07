d19bf0b3b86e307fa36e2bbecac7613c8a187ffd
Dmitry <DMon-git@yandex.ru>
2021-05-07 16:48:21 +0500

    Dashboard page clean.

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -3,18 +3,11 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
+        <div class="col-md-10">
             <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+                <div class="card-body" id="div_user_info">
+                    
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
                 </div>
             </div>
         </div>
