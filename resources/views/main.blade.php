89552beb41c8870d4962c55a3d5765fb0ef756ef
Dmitry <DMon-git@yandex.ru>
2021-05-10 02:02:35 +0500

    Add pagination on view.

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -6,6 +6,15 @@
         <div class="col-md-10">
             <div class="card" id="div_posts">
             </div>
+            <nav aria-label="...">
+              <ul class="pagination pagination-sm">
+                <li class="page-item active" aria-current="page">
+                  <span class="page-link">1</span>
+                </li>
+                <li class="page-item"><a class="page-link" href="#">2</a></li>
+                <li class="page-item"><a class="page-link" href="#">3</a></li>
+              </ul>
+            </nav>
         </div>
     </div>
 </div>
